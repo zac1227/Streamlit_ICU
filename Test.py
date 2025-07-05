1a51f3b99f1129c983e0d36a716e66eb9f315f4f
zac1227 <94905993+zac1227@users.noreply.github.com>
2025-07-05 17:49:53 +0800

    Major file

---
+++ Test.py
@@ -0,0 +1,352 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import shap
+
+# ------------------------- 分頁切換 -------------------------
+model_choice = st.sidebar.radio("請選擇要執行的模型", [
+    "EOMG",
+    "LOMG",
+    "Thymoma",
+    "Non-Thymoma"
+])
+
+# ------------------------- 共用函數：預測 + SHAP -------------------------
+# 共用預測＋SHAP 解釋函式
+def predict_and_explain(model, x_train, input_df, model_name):
+    st.subheader(" 預測結果")
+    
+    try:
+        # 確保順序正確（只保留模型訓練時的特徵）
+        model_feature_names = model.get_booster().feature_names
+        input_df = input_df[model_feature_names]
+        background = x_train[model_feature_names]
+
+
+        # 預測
+        proba = model.predict_proba(input_df)[0]
+        pred_class = int(np.argmax(proba))
+
+        if pred_class == 1:
+            st.success("預測結果：ICU admission")
+        else:
+            st.success("預測結果：Not ICU admission")
+
+        # SHAP 解釋
+        explainer = shap.KernelExplainer(model.predict_proba, background)
+        shap_values = explainer.shap_values(input_df)
+
+        st.subheader("SHAP Waterfall 解釋圖")
+        fig = plt.figure()
+        shap.plots.waterfall(
+            shap.Explanation(
+                values=shap_values[1][0],
+                base_values=explainer.expected_value[1],
+                data=input_df.values[0],
+                feature_names=input_df.columns.tolist()
+            ),
+            
+            show=False
+        )
+        st.pyplot(fig)
+
+    except Exception as e:
+        st.error(f"發生錯誤：{e}")
+
+
+# ------------------------- 模型 A -------------------------
+def run_model_a_page():
+    st.title("EOMG 模型預測頁面")
+    # 模型 & 資料（你之後替換正確路徑）
+    model_dict = pickle.load(open(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\Models\MG_ICU_SHAP_XGB_EOMG.pkl", "rb"))
+    model = model_dict['model']
+    x = pd.read_csv(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\MG_ICU_SHAP_Model_Data_SubGroup2_Age50D_New_FeaName.csv")
+    x_train = x.drop(columns=[ "Y"])
+    # 輸入變數
+    Gender = st.sidebar.number_input("Gender", 1,2)
+    BMI = st.sidebar.number_input("BMI", 10.0, 40.0, 22.5)
+    Infection = st.sidebar.number_input("Infection at admission", 0, 1, 0)
+    Thyroid = st.sidebar.number_input("Thyroid disease", 0, 1, 0)
+    Diabetes = st.sidebar.number_input("Diabetes", 0, 1, 0)
+    Hypertension = st.sidebar.number_input("Hypertension", 0, 1,0)
+    ASCVD = st.sidebar.number_input("ASCVD", 0, 1, 0)
+    Chronic = st.sidebar.number_input("Chronic lung disease", 0, 1, 0)
+    Good = st.sidebar.number_input("Good syndrome", 0, 1, 0)
+    Disease_duration= st.sidebar.number_input("Disease duration (month)", 0, 120, 0)
+    MGFA = st.sidebar.number_input("MGFA clinical classification", 0, 120, 0)
+    Prednisolone = st.sidebar.number_input("Prednisolone daily dose before admission", 0, 100, 0)
+    Immunosuppressant = st.sidebar.number_input("Immunosuppressant at admission", 0, 1, 0)
+    Anti_MuSK = st.sidebar.number_input("Anti-MuSK", 0, 1, 0)
+    Anti_AChR = st.sidebar.number_input("Anti-AChR", 0, 1, 0)
+    dSN = st.sidebar.number_input("dSN", 0, 1, 0)
+    Thymoma = st.sidebar.number_input("Thymoma", 0, 1, 0)
+    Thymic = st.sidebar.number_input("Thymic hyperplasia", 0, 1, 0)
+    Thymectomy = st.sidebar.number_input("Thymectomy", 0, 1, 0)
+    NLR = st.sidebar.number_input("NLR", 0.0, 20.0, 2.0)
+    PLR = st.sidebar.number_input("PLR", 0.0, 1000.0, 100.0)
+    LMR = st.sidebar.number_input("LMR", 0.0, 20.0, 2.0)
+    SII = st.sidebar.number_input("SII", 0.0, 1000.0, 100.0)
+    
+    # 建立 dict（易於維護）
+    input_dict = {
+    "Gender": Gender,
+    "BMI": BMI,
+    "Infection at admission": Infection,
+    "Thyroid disease": Thyroid,
+    "Autoimmune disease": 0, 
+    "Diabetes": Diabetes,
+    "Hypertension": Hypertension,
+    "ASCVD": ASCVD,
+    "Chronic lung disease": Chronic,
+    "Good syndrome": Good,
+    "Disease duration (month)": Disease_duration,
+    "MGFA clinical classification": MGFA,
+    "Prednisolone daily dose before admission": Prednisolone,
+    "Immunosuppressant at admission": Immunosuppressant,
+    "Anti-MuSK": Anti_MuSK,
+    "Anti-AChR": Anti_AChR,
+    "dSN": dSN,
+    "Thymoma": Thymoma,
+    "Thymic hyperplasia": Thymic,
+    "Thymectomy": Thymectomy,
+    "NLR": NLR,
+    "PLR": PLR,
+    "LMR": LMR,
+    "SII": SII
+}
+
+    
+
+
+    if st.sidebar.button("預測模型"):
+        # 用 input_dict 建立 DataFrame
+       # 建立 DataFrame（按照 x_train 的欄位順序）
+        input_df = pd.DataFrame([[input_dict[col] for col in x_train.columns]], columns=x_train.columns)
+        # 印出模型實際特徵
+        model_feature_names = model.get_booster().feature_names
+        
+
+        # 僅保留模型實際特徵
+        input_df = input_df[model_feature_names]
+        
+        predict_and_explain(model, x_train, input_df, "模型 A")
+
+# ------------------------- 模型 B -------------------------
+def run_model_b_page():
+    st.title("LOMG 模型預測頁面")
+    # 模型 & 資料（你之後替換正確路徑）
+    model_dict = pickle.load(open(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\Models\MG_ICU_SHAP_XGB_LOMG.pkl", "rb"))
+    model = model_dict['model']
+    x = pd.read_csv(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\MG_ICU_SHAP_Model_Data_SubGroup2_Age50U_New_FeaName.csv")
+    x_train = x.drop(columns=[ "Y"])
+    # 輸入變數
+    Gender = st.sidebar.number_input("Gender", 1,2)
+    BMI = st.sidebar.number_input("BMI", 10.0, 40.0, 22.5)
+    Infection = st.sidebar.number_input("Infection at admission", 0, 1, 0)
+    Thyroid = st.sidebar.number_input("Thyroid disease", 0, 1, 0)
+    Diabetes = st.sidebar.number_input("Diabetes", 0, 1, 0)
+    Hypertension = st.sidebar.number_input("Hypertension", 0, 1,0)
+    ASCVD = st.sidebar.number_input("ASCVD", 0, 1, 0)
+    Chronic = st.sidebar.number_input("Chronic lung disease", 0, 1, 0)
+    Good = st.sidebar.number_input("Good syndrome", 0, 1, 0)
+    Disease_duration= st.sidebar.number_input("Disease duration (month)", 0, 120, 0)
+    MGFA = st.sidebar.number_input("MGFA clinical classification", 0, 120, 0)
+    Prednisolone = st.sidebar.number_input("Prednisolone daily dose before admission", 0, 100, 0)
+    Immunosuppressant = st.sidebar.number_input("Immunosuppressant at admission", 0, 1, 0)
+    Anti_MuSK = st.sidebar.number_input("Anti-MuSK", 0, 1, 0)
+    Anti_AChR = st.sidebar.number_input("Anti-AChR", 0, 1, 0)
+    dSN = st.sidebar.number_input("dSN", 0, 1, 0)
+    Thymoma = st.sidebar.number_input("Thymoma", 0, 1, 0)
+    Thymic = st.sidebar.number_input("Thymic hyperplasia", 0, 1, 0)
+    Thymectomy = st.sidebar.number_input("Thymectomy", 0, 1, 0)
+    NLR = st.sidebar.number_input("NLR", 0.0, 20.0, 2.0)
+    PLR = st.sidebar.number_input("PLR", 0.0, 1000.0, 100.0)
+    LMR = st.sidebar.number_input("LMR", 0.0, 20.0, 2.0)
+    SII = st.sidebar.number_input("SII", 0.0, 1000000000.0, 100.0)
+    
+    # 建立 dict（易於維護）
+    input_dict = {
+    "Gender": Gender,
+    "BMI": BMI,
+    "Infection at admission": Infection,
+    "Thyroid disease": Thyroid,
+    "Autoimmune disease": 0, 
+    "Diabetes": Diabetes,
+    "Hypertension": Hypertension,
+    "ASCVD": ASCVD,
+    "Chronic lung disease": Chronic,
+    "Good syndrome": Good,
+    "Disease duration (month)": Disease_duration,
+    "MGFA clinical classification": MGFA,
+    "Prednisolone daily dose before admission": Prednisolone,
+    "Immunosuppressant at admission": Immunosuppressant,
+    "Anti-MuSK": Anti_MuSK,
+    "Anti-AChR": Anti_AChR,
+    "dSN": dSN,
+    "Thymoma": Thymoma,
+    "Thymic hyperplasia": Thymic,
+    "Thymectomy": Thymectomy,
+    "NLR": NLR,
+    "PLR": PLR,
+    "LMR": LMR,
+    "SII": SII
+}
+    if st.sidebar.button("預測模型"):
+        # 用 input_dict 建立 DataFrame
+       # 建立 DataFrame（按照 x_train 的欄位順序）
+        input_df = pd.DataFrame([[input_dict[col] for col in x_train.columns]], columns=x_train.columns)
+        # 印出模型實際特徵
+        model_feature_names = model.get_booster().feature_names
+        
+
+        # 僅保留模型實際特徵
+        input_df = input_df[model_feature_names]
+        
+        predict_and_explain(model, x_train, input_df, "模型 B")
+    
+        
+# ------------------------- 模型 B -------------------------
+def run_model_c_page():
+    st.title("Thymoma 模型預測頁面")
+    # 模型 & 資料（你之後替換正確路徑）
+    model_dict = pickle.load(open(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\Models\MG_ICU_SHAP_XGB_Thymoma.pkl", "rb"))
+    model = model_dict['model']
+    x = pd.read_csv(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\MG_ICU_SHAP_Model_Data_SubGroup1_X9_1_FeaName.csv")
+    x_train = x.drop(columns=[ "Y"])
+    # 輸入變數
+    Gender = st.sidebar.number_input("Gender", 1,2)
+    Age= st.sidebar.number_input("Age at onset (year)", 0, 100, 1)
+    BMI = st.sidebar.number_input("BMI", 10.0, 40.0, 22.5)
+    Recurrent_thymoma = st.sidebar.number_input("Recurrent thymoma", 0, 1, 0)
+    Invasive_thymoma = st.sidebar.number_input("Invasive thymoma", 0, 1, 0)
+    Infection = st.sidebar.number_input("Infection at admission", 0, 1, 0)
+    Thyroid = st.sidebar.number_input("Thyroid disease", 0, 1, 0)
+    Autoimmune= st.sidebar.number_input("Autoimmune disease", 0, 1, 0)
+    Diabetes = st.sidebar.number_input("Diabetes", 0, 1, 0)
+    Hypertension = st.sidebar.number_input("Hypertension", 0, 1,0)
+    ASCVD = st.sidebar.number_input("ASCVD", 0, 1, 0)
+    Chronic = st.sidebar.number_input("Chronic lung disease", 0, 1, 0)
+    Good = st.sidebar.number_input("Good syndrome", 0, 1, 0)
+    Disease_duration= st.sidebar.number_input("Disease duration (month)", 0, 120, 0)
+    Prednisolone = st.sidebar.number_input("Prednisolone daily dose before admission", 0, 100, 0)
+    Immunosuppressant = st.sidebar.number_input("Immunosuppressant at admission", 0, 1, 0)
+    NLR = st.sidebar.number_input("NLR", 0.0, 20.0, 1.0)
+    PLR = st.sidebar.number_input("PLR", 0.0, 1000.0, 100.0)
+    LMR = st.sidebar.number_input("LMR", 0.0, 10.0, 1.0)
+    SII = st.sidebar.number_input("SII", 0.0, 10000000.0, 100.0)
+    
+    # 建立 dict（易於維護）
+    input_dict = {
+    "Gender": Gender,
+    "Age at onset (year)": Age,
+    "BMI": BMI,
+    "Recurrent thymoma": Recurrent_thymoma,
+    "Invasive thymoma": Invasive_thymoma,
+    "Infection at admission": Infection,
+    "Thyroid disease": Thyroid,
+    "Autoimmune disease": Autoimmune, 
+    "Diabetes": Diabetes,
+    "Hypertension": Hypertension,
+    "ASCVD": ASCVD,
+    "Chronic lung disease": Chronic,
+    "Good syndrome": Good,
+    "Disease duration (month)": Disease_duration,
+    "Prednisolone daily dose before admission": Prednisolone,
+    "Immunosuppressant at admission": Immunosuppressant,
+    "NLR": NLR,
+    "PLR": PLR,
+    "LMR": LMR,
+    "SII": SII
+}
+    if st.sidebar.button("預測模型"):
+        # 用 input_dict 建立 DataFrame
+       # 建立 DataFrame（按照 x_train 的欄位順序）
+        input_df = pd.DataFrame([[input_dict[col] for col in x_train.columns]], columns=x_train.columns)
+        # 印出模型實際特徵
+        model_feature_names = model.get_booster().feature_names
+        
+
+        # 僅保留模型實際特徵
+        input_df = input_df[model_feature_names]
+        
+        predict_and_explain(model, x_train, input_df, "模型 C")        
+# ------------------------- 模型 B -------------------------
+def run_model_d_page():
+    st.title("NonThymoma 模型預測頁面")
+    # 模型 & 資料（你之後替換正確路徑）
+    model_dict = pickle.load(open(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\Models\MG_ICU_SHAP_XGB_NonThymoma.pkl", "rb"))
+    model = model_dict['model']
+    x = pd.read_csv(r"C:\Users\a0986\Streamlit_Project\ICU\Streamlit\MG_ICU_SHAP_Model_Data_SubGroup1_X9_0_FeaName.csv")
+    x_train = x.drop(columns=[ "Y"])
+    # 輸入變數
+    Gender = st.sidebar.number_input("Gender", 1,2)
+    Age= st.sidebar.number_input("Age at onset (year)", 0, 100, 1)
+    BMI = st.sidebar.number_input("BMI", 10.0, 40.0, 22.5)
+    Recurrent_thymoma = st.sidebar.number_input("Recurrent thymoma", 0, 1, 0)
+    Invasive_thymoma = st.sidebar.number_input("Invasive thymoma", 0, 1, 0)
+    Infection = st.sidebar.number_input("Infection at admission", 0, 1, 0)
+    Thyroid = st.sidebar.number_input("Thyroid disease", 0, 1, 0)
+    Autoimmune= st.sidebar.number_input("Autoimmune disease", 0, 1, 0)
+    Diabetes = st.sidebar.number_input("Diabetes", 0, 1, 0)
+    Hypertension = st.sidebar.number_input("Hypertension", 0, 1,0)
+    ASCVD = st.sidebar.number_input("ASCVD", 0, 1, 0)
+    Chronic = st.sidebar.number_input("Chronic lung disease", 0, 1, 0)
+    Good = st.sidebar.number_input("Good syndrome", 0, 1, 0)
+    Disease_duration= st.sidebar.number_input("Disease duration (month)", 0, 120, 0)
+    Prednisolone = st.sidebar.number_input("Prednisolone daily dose before admission", 0, 100, 0)
+    Immunosuppressant = st.sidebar.number_input("Immunosuppressant at admission", 0, 1, 0)
+    NLR = st.sidebar.number_input("NLR", 0.0, 20.0, 1.0)
+    PLR = st.sidebar.number_input("PLR", 0.0, 1000.0, 10.0)
+    LMR = st.sidebar.number_input("LMR", 0.0, 10.0, 1.0)
+    SII = st.sidebar.number_input("SII", 0.0, 10000000.0, 100.0)
+    
+    # 建立 dict（易於維護）
+    input_dict = {
+    "Gender": Gender,
+    "Age at onset (year)": Age,
+    "BMI": BMI,
+    "Recurrent thymoma": Recurrent_thymoma,
+    "Invasive thymoma": Invasive_thymoma,
+    "Infection at admission": Infection,
+    "Thyroid disease": Thyroid,
+    "Autoimmune disease": Autoimmune, 
+    "Diabetes": Diabetes,
+    "Hypertension": Hypertension,
+    "ASCVD": ASCVD,
+    "Chronic lung disease": Chronic,
+    "Good syndrome": Good,
+    "Disease duration (month)": Disease_duration,
+    "Prednisolone daily dose before admission": Prednisolone,
+    "Immunosuppressant at admission": Immunosuppressant,
+    "NLR": NLR,
+    "PLR": PLR,
+    "LMR": LMR,
+    "SII": SII
+}
+    if st.sidebar.button("預測模型"):
+        # 用 input_dict 建立 DataFrame
+       # 建立 DataFrame（按照 x_train 的欄位順序）
+        input_df = pd.DataFrame([[input_dict[col] for col in x_train.columns]], columns=x_train.columns)
+        # 印出模型實際特徵
+        model_feature_names = model.get_booster().feature_names
+        
+
+        # 僅保留模型實際特徵
+        input_df = input_df[model_feature_names]
+        
+        predict_and_explain(model, x_train, input_df, "模型 D") 
+
+
+# ------------------------- 主控制邏輯 -------------------------
+if model_choice == "EOMG":
+    run_model_a_page()
+elif model_choice == "LOMG":
+    run_model_b_page()
+elif model_choice == "Thymoma":
+    run_model_c_page()
+elif model_choice == "Non-Thymoma":
+    run_model_d_page()
+
+
