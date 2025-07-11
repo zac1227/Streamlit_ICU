79b4041e4eb5ea1c623c6f33c4aa1353cfcd3a07
zac1227 <a0986710233@gmail.com>
2025-07-11 20:46:29 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -153,12 +153,91 @@ def run_model_a_page():
         
         predict_and_explain(model, x_train, input_df, "模型 A")
 
+# ------------------------- 模型 B -------------------------
+
+def run_model_b_page():
+    st.title("LOMG 模型預測頁面")
+    # 模型 & 資料（你之後替換正確路徑）
+    import xgboost as xgb
+    model = xgb.XGBClassifier()
+    model.load_model(r"MG_ICU_SHAP_XGB_LOMG.json")
+    x = pd.read_csv(r"MG_ICU_SHAP_Model_Data_SubGroup2_Age50U_New_FeaName.csv")
+    x_train = x.drop(columns=[ "Y","MGFA clinical classification"])
+    # 輸入變數
+    Gender = st.sidebar.radio("Gender", options=[1, 2])
+    BMI = st.sidebar.number_input("BMI", 10.0, 50.0, 22.5)
+    Infection = st.sidebar.radio("Infection at admission", options=[0, 1])  # 0 = No, 1 = Yes
+    Thyroid = st.sidebar.radio("Thyroid disease", options=[0, 1])  # 0 = No, 1 = Yes
+    Auto = st.sidebar.radio("Autoimmune disease", options=[0, 1])  # 0 = No, 1 = Yes
+    Diabetes = st.sidebar.radio("Diabetes", options=[0, 1])  # 0 = No, 1 = Yes
+    Hypertension = st.sidebar.radio("Hypertension", options=[0, 1])  # 0 = No, 1 = Yes
+    ASCVD = st.sidebar.radio("ASCVD", options=[0, 1])  # 0 = No, 1 = Yes
+    Chronic = st.sidebar.radio("Chronic lung disease", options=[0, 1])  # 0 = No, 1 = Yes
+    Good = st.sidebar.radio("Good syndrome", options=[0, 1])  # 0 = No, 1 = Yes
+    Disease_duration= st.sidebar.number_input("Disease duration (month)", 0, 120, 0)
+
+    Prednisolone = st.sidebar.number_input("Prednisolone daily dose before admission", 0, 100, 0)
+    Immunosuppressant = st.sidebar.number_input("Immunosuppressant at admission", 0, 3, 0)
+    Anti_MuSK = st.sidebar.radio("Anti-MuSK", options=[0, 1])  # 0 = No, 1 = Yes
+    Anti_AChR = st.sidebar.radio("Anti-AChR", options=[0, 1])  # 0 = No, 1 = Yes
+    dSN = st.sidebar.radio("dSN", options=[0, 1])  # 0 = No, 1 = Yes
+    Thymoma = st.sidebar.number_input("Thymoma", 0, 4, 0)
+    Thymic = st.sidebar.radio("Thymic hyperplasia", options=[0, 1])  # 0 = No, 1 = Yes
+    Thymectomy = st.sidebar.number_input("Thymectomy", 0, 3, 0)
+
+    NLR = st.sidebar.number_input("NLR", 0.0, 100.0, 0.0)
+    PLR = st.sidebar.number_input("PLR", 0.0, 1000.0, 0.0)
+    LMR = st.sidebar.number_input("LMR", 0.0, 20.0, 0.0)
+    SII = st.sidebar.number_input("SII", 0.0, 10000000.0, 0.0)
+    
+    # 建立 dict（易於維護）
+    input_dict = {
+    "Gender": Gender,
+    "BMI": BMI,
+    "Infection at admission": Infection,
+    "Thyroid disease": Thyroid,
+    "Autoimmune disease": Auto, 
+    "Diabetes": Diabetes,
+    "Hypertension": Hypertension,
+    "ASCVD": ASCVD,
+    "Chronic lung disease": Chronic,
+    "Good syndrome": Good,
+    "Disease duration (month)": Disease_duration,
+    "MGFA clinical classification": 0,
+    "Prednisolone daily dose before admission": Prednisolone,
+    "Immunosuppressant at admission": Immunosuppressant,
+    "Anti-MuSK": Anti_MuSK,
+    "Anti-AChR": Anti_AChR,
+    "dSN": dSN,
+    "Thymoma": Thymoma,
+    "Thymic hyperplasia": Thymic,
+    "Thymectomy": Thymectomy,
+    "NLR": NLR,
+    "PLR": PLR,
+    "LMR": LMR,
+    "SII": SII
+}
+
+    if st.sidebar.button("預測模型"):
+        # 用 input_dict 建立 DataFrame
+       # 建立 DataFrame（按照 x_train 的欄位順序）
+        input_df = pd.DataFrame([[input_dict[col] for col in x_train.columns]], columns=x_train.columns)
+        # 印出模型實際特徵
+        model_feature_names = model.get_booster().feature_names
+        
+
+        # 僅保留模型實際特徵
+        input_df = input_df[model_feature_names]
+        
+        predict_and_explain(model, x_train, input_df, "模型 B")
 
 
 # ------------------------- 主控制邏輯 -------------------------
 
 if model_choice == "EOMG":
     run_model_a_page()
+elif model_choice == "LOMG":
+    run_model_b_page()
 
 
    
